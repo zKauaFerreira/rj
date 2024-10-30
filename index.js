f8bfd30d2d89c7fb15205f203e252c75f770047a
Kauã Codex <128728370+kauacodex@users.noreply.github.com>
2024-10-30 16:30:54 -0300

    V2

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const path = require('path');
 const apiUrl = 'https://portal1.snirh.gov.br/server/rest/services/SGH/CotasReferencia2/MapServer/dynamicLayer/query?f=json&returnGeometry=false&spatialRel=esriSpatialRelIntersects&geometry=%7B"xmin"%3A-5704261.757328038%2C"ymin"%3A-3508025.0787798706%2C"xmax"%3A-5704204.429556822%2C"ymax"%3A-3507967.751008655%2C"spatialReference"%3A%7B"wkid"%3A102100%7D%7D&geometryType=esriGeometryEnvelope&inSR=102100&outFields=Data_ult_dado%2CUlt_Dado&outSR=102100&layer=%7B"source"%3A%7B"type"%3A"mapLayer"%2C"mapLayerId"%3A2%7D%7D';
 const outputFilePath = path.join(process.cwd(), 'dados.json'); // Salva na raiz do repositório
 
-
 // Função para formatar a data
 function formatDate(date) {
     const day = String(date.getDate()).padStart(2, '0');
@@ -14,8 +13,9 @@ function formatDate(date) {
     const year = date.getFullYear();
     const hours = String(date.getHours()).padStart(2, '0');
     const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
 
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
 
 // Função para buscar os dados
@@ -66,7 +66,6 @@ async function startCron() {
         if (timeRemaining % 60000 === 0) {
             console.clear();
             console.log(`Próxima atualização em: ${minutes}m`);
-            
         }
 
         // Quando o tempo se esgota, busca os dados novamente e reinicia o cronômetro
